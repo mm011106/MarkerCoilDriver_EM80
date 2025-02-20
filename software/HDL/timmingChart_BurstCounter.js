989361c5c15d7101dd431c3c17bfb695a6209625
mm011106 <mm011106@gmail.com>
2025-02-20 15:02:50 +0900

    タイミングチャート作成

---
+++ software/HDL/timmingChart_BurstCounter.js
@@ -0,0 +1,17 @@
+{signal: [
+    {name: 'CLK(WAVE_CARRY)', wave: 'P..........'},
+    {name: 'BURST_CNT', wave: '22222222222', data:['5','0','1', '2', '3', '4','5','0','1', '2', '3'],phase:0},
+    //  {name: 'WAVE_CNT', wave: 'x12345x|=.x', data: ['head', 'body', 'tail', 'data']},
+    {name: 'BLOCK_SYNC', wave: '010....10..', phase:0},
+    //  {},
+    {name: 'BLOCK_MASK', wave: '10....10...'}
+  ],
+  
+  head:{
+    text:'BURST_COUNTER 動作'
+  }
+// foot:{
+//   text:'Figure 100',
+// }
+
+}
---
+++ software/HDL/timmingChart_WaveCounter.js
@@ -0,0 +1,17 @@
+{signal: [
+    {name: 'CLK', wave: 'P.....|.....'},
+    {name: 'WAVE_CNT', wave: '222222|22222', data:['24','0','1', '2', '3', '','23', '24', '0','1', '2', '3',],phase:0},
+    //  {name: 'WAVE_CNT', wave: 'x12345x|=.x', data: ['head', 'body', 'tail', 'data']},
+    {name: 'WAVE_SYNC', wave: '0.10..|...10', phase:0},
+    //  {},
+    {name: 'WAVE_CARRY', wave: '010...|..10.'}
+  ],
+  
+  head:{
+    text:'WAVE_COUNTER 動作'
+  }
+// foot:{
+//   text:'Figure 100',
+// }
+
+}
